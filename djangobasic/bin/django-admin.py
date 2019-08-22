bf5a6c8eff78c3989e87faf1f1d3ea8778a66561
swaroop2000 <kkrishnaswaroop99@gmail.com>
2019-08-22 09:58:12 +0530

    First Commit

---
+++ djangobasic/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/krishna/Coding/Dev/Development/Django/Django-Apps/djangobasic/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
