55018eb47f52d8e09f43626cc0c9b2936d877de9
swaroop2000 <kkrishnaswaroop99@gmail.com>
2019-08-22 19:36:59 +0530

    Added HTML

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+#post_list is a function which takes a request and returns back another function
+def post_list(request):
+	return render(request, 'blog/post_list.html',{})
+
