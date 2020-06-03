017679a5b9cdbe35e360bafc3df9af2616aa6999
geekswaroop <kkrishnaswaroop99@gmail.com>
2020-06-03 16:10:37 +0530

    basic templating

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,8 +2,35 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+# def home(request):
+#     return HttpResponse('<h1>Welcome to the blog!</h1>')
+
+# def about(request):
+#     return HttpResponse('<h1>About Page of the blog</h1>')
+
+posts = [
+    {
+    'author': 'Krishna Swaroop',
+    'title': 'Blog Post 1',
+    'date_posted': 'June 3, 2020',
+    'content': 'First Blog post content'
+    },
+    {
+    'author': 'John Doe',
+    'title': 'Blog Post 2',
+    'date_posted': 'June 3, 2020',
+    'content': 'Second Blog post content'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Welcome to the blog!</h1>')
+    context = {
+        'key': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>About Page of the blog</h1>')
+    jsonDict = {
+        'title': 'About'
+    }
+    return render(request, 'blog/about.html', jsonDict)
